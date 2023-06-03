e7dbc60642b93f6845981b9146c4549f14713bc8
tbot <thibaut.endress27@proton.me>
2023-06-03 12:57:40 +0200

    Added url import to GeopandasDataframe

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("This is my first commit!")
+import geopandas as gpd
+import requests
+
+url = "https://bikeshare.metro.net/stations/json/"
+r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+"""Have to import using requests because the geopandas.read_file(url) doesn't allow for
+specifying a request header, which results in a 403 denied response"""
+
+data = r.json()
+gdf = gpd.GeoDataFrame.from_features(data)
+print(gdf)    
