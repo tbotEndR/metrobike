b5f702f1926b6d73d025072d93ec6c0ffa374c33
tbot <thibaut.endress27@proton.me>
2023-06-09 12:04:03 +0200

    added dist calc function

--- test.py
+++ test.py
@@ -3,31 +3,9 @@ import folium
 import geopandas as gpd
 import requests
 import webbrowser
+from shapely.geometry import Point
+import math
 
-<<<<<<< HEAD
-#G = ox.graph_from_bbox(34.1879, 33.8983, -118.1576, -118.5287, network_type='bike')
-#ox.save_graphml(G, "LA.graphml")
-G = ox.load_graphml("LA.graphml")
-fig, ax = ox.plot_graph(G, figsize=(10, 10), node_size=0, edge_color='y', edge_linewidth=0.2)
-print(G)
-
-url = "https://bikeshare.metro.net/stations/json/"
-r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-#GeoJSON import using requests because the geopandas.read_file(url) doesn't allow for
-##specifying a request header, which results in a 403 denied response
-
-data = r.json()
-fulldf = gpd.GeoDataFrame.from_features(data)
-workdf = fulldf[["addressStreet", "addressZipCode", "bikesAvailable", "docksAvailable", "latitude", "longitude"]]
-workdf['distToUser'] = 'Null'
-
-#m = folium.Map(location=[workdf.latitude.mean(), workdf.longitude.mean()], zoom_start=14, control_scale=True)
-
-#I know iterating over pandas data frames is bad practide but I'm tired and just need this to work right now
-for i, row in workdf.iterrows():
-    stationNode = (row['latitude'], row['longitude'])
-    ox.get_near
-=======
 class Map:
     def __init__(self, center, zoom_start):
         self.center = center
@@ -56,7 +34,11 @@ def getStationData():
     ##specifying a request header, which results in a 403 denied response
     data = r.json()
     fulldf = gpd.GeoDataFrame.from_features(data)
-    return fulldf[["addressStreet", "addressZipCode", "bikesAvailable", "docksAvailable", "latitude", "longitude"]]
+    newdf = fulldf[["addressStreet", "addressZipCode", "bikesAvailable", "docksAvailable", "latitude", "longitude"]]
+    newdf.assign({'userDist' : 0.0})
+
+def calcDist(point1, point2):
+    return (math.acos*(math.sin(point1.x)*(math.sin(point2.x))+ math.cos(point1.x)*math.cos(point2.x)*math.cos(point2.y-point1.y))*6371)
 
 
 stationDf = getStationData()
@@ -65,4 +47,3 @@ print(stationDf)
 m = folium.Map(location=[stationDf.latitude.mean(), stationDf.longitude.mean()], zoom_start=14, control_scale=True)
 m.save("bigMap.html")
 webbrowser.open("bigMap.html")
->>>>>>> addUserInput
