65147f78e9d483bed6c1d3bba2147c94b3c557cf
tbot <thibaut.endress27@proton.me>
2023-06-09 11:41:41 +0200

    added ignore, code formatting

--- test.py
+++ test.py
@@ -18,24 +18,26 @@ def getUserBikes():
     userLoc = input("What is your current location? (Default central LA)\n") or "Mid-City, Los Angeles"
     kBikeStations = int(input("How many bikestations are you seaching for? (Default 1)\n") or 1)
 
-try:
-    G = ox.load_graphml("LA.graphml")
-except FileNotFoundError:
-    G = ox.graph_from_bbox(34.1879, 33.8983, -118.1576, -118.5287, network_type='bike')
-    ox.save_graphml(G, "LA.graphml")
+def getNetworkGrid():
+    try:
+        G = ox.load_graphml("LA.graphml")
+    except FileNotFoundError:
+        G = ox.graph_from_bbox(34.1879, 33.8983, -118.1576, -118.5287, network_type='bike')
+        ox.save_graphml(G, "LA.graphml")
 
-url = "https://bikeshare.metro.net/stations/json/"
-r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-#GeoJSON import using requests because the geopandas.read_file(url) doesn't allow for
-##specifying a request header, which results in a 403 denied response
+def getStationData():
+    url = "https://bikeshare.metro.net/stations/json/"
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    #GeoJSON import using requests because the geopandas.read_file(url) doesn't allow for
+    ##specifying a request header, which results in a 403 denied response
+    data = r.json()
+    fulldf = gpd.GeoDataFrame.from_features(data)
+    return fulldf[["addressStreet", "addressZipCode", "bikesAvailable", "docksAvailable", "latitude", "longitude"]]
 
-data = r.json()
-fulldf = gpd.GeoDataFrame.from_features(data)
-workdf = fulldf[["addressStreet", "addressZipCode", "bikesAvailable", "docksAvailable", "latitude", "longitude"]]
-#workdf['distToUser'] = Null
 
-print(workdf)
+stationDf = getStationData()
+print(stationDf)
 
-m = folium.Map(location=[workdf.latitude.mean(), workdf.longitude.mean()], zoom_start=14, control_scale=True)
+m = folium.Map(location=[stationDf.latitude.mean(), stationDf.longitude.mean()], zoom_start=14, control_scale=True)
 m.save("bigMap.html")
 webbrowser.open("bigMap.html")
