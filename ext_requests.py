a7c0051b91d3465183467f58e91c95b9f77252ae
Thibaut Endreß <133337412+tbotEndR@users.noreply.github.com>
2023-06-16 16:04:52 +0200

    insert key message

--- ext_requests.py
+++ ext_requests.py
@@ -36,7 +36,7 @@ def directions(startLat, startLon, destLat, destLon, mode="bicycle"):
     url = "https://route-and-directions.p.rapidapi.com/v1/routing"
     querystring = {"waypoints":f"{str(startLat)},{str(startLon)}|{str(destLat)},{str(destLon)}", "mode":mode}
     headers = {
-	"X-RapidAPI-Key": "",
+	"X-RapidAPI-Key": "INSERT YOUR KEY HERE",
 	"X-RapidAPI-Host": "route-and-directions.p.rapidapi.com"
     }
     response = requests.get(url, headers=headers, params=querystring)
