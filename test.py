772782bf1b15a034bb83548962cee8c8cf329252
tbotEndR <thibaut.endress27@proton.me>
2023-06-16 13:40:22 +0200

    restructured functions, added route plotting

--- test.py
+++ test.py
@@ -14,6 +14,11 @@ class locationObj:
     def __init__(self, name, latitude, longitude):
         self.name = name
         self.coordinates = (latitude, longitude)
+    
+    def latitude(self):
+        return self.coordinates[0]
+    def longitude(self):
+        return self.coordinates[1]
 
 class stationObj(locationObj):
     def __init__(self, name, latitude, longitude, bikes, docks, userDist=None):
@@ -24,7 +29,7 @@ class stationObj(locationObj):
         self.userDist = userDist
 
 class userObj(locationObj):
-    def __init__(self, name, latitude, longitude, k, queryType):
+    def __init__(self, name, latitude, longitude, k=1, queryType=None):
         self.name = name
         self.coordinates = (latitude, longitude)
         self.k = k
@@ -34,32 +39,53 @@ class routeObj():
     def __init__(self, source, destination):
         self.source = source
         self.destination = destination
-        self.startStation = source.nearestStation("bikes")[0]
-        self.endStation = destination.nearestStation("docks")[0]
+        self.sourceStation = nearestStation(source.coordinates, 1, stationType="bikes")[0]
+        self.destinationStation = nearestStation(destination.coordinates, 1, stationType="docks")[0]
 
 def do_geocode(address, attempt = 1, max_attempts = 5):
     try:
         return geocode(address)
+    
     except exc.GeocoderTimedOut:
         if attempt <= max_attempts:
             return do_geocode(address, attempt=attempt+1)
-        raise
+        exit("Geocoding service requests timed out. Please try again.\n")
 
-def getUserInput():
-    userAddress= input("What is your current location?\n") or "Mid-City, Los Angeles"
-    numOfStations = int(input("How many stations do you want to display? (Default 1)\n") or 1)
-    queryType = input("Are you searching for available bikes, docks or do you need route planning? (-b, -d or -r)\n").lower() or "-b"
-    userLocation = do_geocode(userAddress)
-    if userLocation == None:
-        exit("Location not found.")
-    else:
-        if queryType == "-b":
-            user = userObj("You are here", userLocation.latitude, userLocation.longitude, numOfStations, "bikes")
-        elif queryType== "-d":
-            user = userObj("You are here", userLocation.latitude, userLocation.longitude, numOfStations, "docks")
-        else:
-            exit("Couldn't parse bikes or stations.")
-        return user
+    except TimeoutError:
+        if attempt <= max_attempts:
+            return do_geocode(address, attempt=attempt+1)
+        exit("Geocoding service requests timed out. Please try again.\n")
+
+
+def getUserLocation(arg = "station"):
+    userLocation = None
+    if arg == "station":
+        while userLocation is None:
+            userAddress= input("What is your current location? (Default: Mid-City, LA)\n") or "Mid-City, Los Angeles"
+            userLocation = do_geocode(userAddress)
+            if userLocation == None:
+                print("Location not found. Try again.\n")
+
+    elif arg == "source":
+        while userLocation is None:
+            userAddress= input("What is your starting point?\n")
+            userLocation = do_geocode(userAddress)
+            if userLocation == None:
+                print("Location not found. Try again.\n")
+    
+    elif arg == "destination":
+        while userLocation is None:
+            userAddress= input("What is your destination?\n")
+            userLocation = do_geocode(userAddress)
+            if userLocation == None:
+                print("Location not found. Try again.\n")
+
+    print(userLocation)
+    print("Is this the correct location? (y/n)")
+    correct = input() or 'y'
+    if correct == 'n':
+        getUserLocation(arg)
+    return userLocation
 
 def calcDist(stationLoc, userLoc):
     try:
@@ -73,7 +99,7 @@ def nearestStation(location, k = 1, stationType = "bikes"):
                 for stationLat, stationLon in tmpdf[["latitude", "longitude"]].values]
     if stationType == "bikes":
         tmpdf = tmpdf.drop(tmpdf[tmpdf["bikesAvailable"] == 0].index)
-    elif stationType == "bikes":
+    elif stationType == "docks":
         tmpdf = tmpdf.drop(tmpdf[tmpdf["docksAvailable"] == 0].index)
     tmpdf = tmpdf.sort_values("userDist", ascending=True)
     tmpdf = tmpdf.reset_index(drop=True)
@@ -95,19 +121,99 @@ def getStationData():
     smalldf = smalldf.assign(userDist='NV')
     return smalldf
 
+def toMarker(station, type):
+    if type == "bikes":
+        html = """{0}<br><br>Available bikes: {1}""".format(station.name, station.bikes)
+        iconType = "bicycle"
+        color = "darkblue"
+    elif type == "docks":
+        html = """{0}<br><br>Available docks: {1}""".format(station.name, station.docks)
+        iconType = "charging-station"
+        color = "lightred"
+    iframe = folium.IFrame(html, figsize=(3,2))
+    popup = folium.Popup(iframe, max_width=200)
+    mapMarker = folium.Marker(location=station.coordinates, popup=popup, icon=folium.Icon(color=color, prefix='fa', icon=iconType))
+    return mapMarker
 
+def nearestX(arg, map):
+    userLoc = getUserLocation()
+    if arg == "bikes":
+        k = int(input("How many bike stations do you want to display? (Default: 1)\n")) or 1
+    elif arg == "docks":
+        k = int(input("How many docks do you want to display? (Default: 1)\n")) or 1
+
+    user = userObj(name = "You are here", latitude= userLoc.latitude, longitude= userLoc.longitude, k = k, queryType = arg)
+    relevantStations = nearestStation(user.coordinates, user.k, user.queryType)
+    stationCluster = MarkerCluster(name="nearest stations",).add_to(map)
+    for i in range(len(relevantStations)):
+        toMarker(relevantStations[i], user.queryType).add_to(stationCluster)
+    folium.Marker(location=user.coordinates, popup="You are here", icon=folium.Icon(color="green", prefix='fa', icon="circle-user")).add_to(map)
+    map.save("bigMap.html")
+
+def directions(startLat, startLon, destLat, destLon, mode="bicycle"):
+    url = "https://route-and-directions.p.rapidapi.com/v1/routing"
+    querystring = {"waypoints":f"{str(startLat)},{str(startLon)}|{str(destLat)},{str(destLon)}", "mode":mode}
+    #querystring = {"waypoints":"34.06339,-118.23616|33.99537,-118.4648","mode":"walk"}
+    headers = {
+	"X-RapidAPI-Key": "f5a6ad24fdmsh40bd4fffc865494p135019jsndaa4d4c1bd9b",
+	"X-RapidAPI-Host": "route-and-directions.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers, params=querystring)
+    return response
+
+def plotRoute(routeResponse, map):
+    mls = routeResponse.json()['features'][0]['geometry']['coordinates']
+    points = [(i[1], i[0]) for i in mls[0]]
+    folium.PolyLine(points, weight=5, opacity=1).add_to(map)
+
+
+def routePlanning(map):
+    source = getUserLocation("source")
+    srcObj = locationObj(source.address, source.latitude, source.longitude)
+    destination = getUserLocation("destination")
+    destObj = locationObj(destination.address, destination.latitude, destination.longitude)
+    route = routeObj(srcObj, destObj)
+    bikeStation = route.sourceStation
+    dockStation = route.destinationStation
+    print(bikeStation.latitude(), bikeStation.longitude(), dockStation.latitude(), dockStation.longitude())
+
+    startToBike = directions(srcObj.latitude(), srcObj.longitude(), bikeStation.latitude(), bikeStation.longitude(), mode="walk")
+    bikeToDock = directions(bikeStation.latitude(), bikeStation.longitude(), dockStation.latitude(), dockStation.longitude())
+    dockToDest = directions(dockStation.latitude(), dockStation.longitude(), destObj.latitude(), destObj.longitude(), mode="walk")
+
+    print(startToBike)
+
+    plotRoute(startToBike, map)
+    plotRoute(bikeToDock, map)
+    plotRoute(dockToDest, map)
+
+    toMarker(route.sourceStation, "bikes").add_to(map)
+    toMarker(route.destinationStation, "docks").add_to(map)
+    folium.Marker(location=route.source.coordinates, popup="Start", icon=folium.Icon(color="green", prefix='fa', icon="circle-user")).add_to(map)
+    folium.Marker(location=route.destination.coordinates, popup="Destination", icon=folium.Icon(color="green", prefix='fa', icon="map-pin")).add_to(map)
+    map.save("bigMap.html")
+    
+    
+
+"""Setup code, grab newest GeoJSON Data and turn it into a geopandas df with only the relevant information"""
 geolocator = Nominatim(user_agent="metrobike_application")
 geocode = lambda query: geolocator.geocode("%s, Los Angeles County CA" % query)
 workingdf = getStationData()
+m = folium.Map(location=[workingdf.latitude.mean(), workingdf.longitude.mean()], zoom_start=12, control_scale=True)
+minimap = folium.plugins.MiniMap()
+m.add_child(minimap)
+print("<<Loading in current data>>\n")
 
-user = getUserInput()
-stationDf = getStationData()
-relevantStations = nearestStation(user.coordinates, user.k, user.queryType)
+"""Menu"""
+queryType = input("Are you searching for available bikes (-b), docks (-d) or do you need route planning (-r)?\n").lower() or "-b"
+if queryType == "-b":
+    nearestX("bikes", m) 
+elif queryType== "-d":
+    nearestX("docks", m)
+elif queryType == "-r":
+    routePlanning(m)
+else:
+    exit("Couldn't parse your input, please try again.")
 
-m = folium.Map(location=[stationDf.latitude.mean(), stationDf.longitude.mean()], zoom_start=12, control_scale=True)
-stationCluster = MarkerCluster(name="nearest stations",).add_to(m)
-for i in range(len(relevantStations)):
-    folium.Marker(location=relevantStations[i].coordinates, popup=relevantStations[i].name, icon=folium.Icon(color="blue")).add_to(stationCluster)
-folium.Marker(location=user.coordinates, popup=user.name, icon=folium.Icon(color="green")).add_to(m)
-m.save("bigMap.html")
 webbrowser.open("bigMap.html")
+exit("\nThank you for using our services.\n")
